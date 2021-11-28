2e7f2b2baccbfae6cfc35b4b4c43a016aa8f5ca8
Ranjeetk489 <49064698+Ranjeetk489@users.noreply.github.com>
2021-11-28 20:06:45 +0530

    Uploaded build file

---
+++ build/precache-manifest.5277c141ec690d0224307eefc6deac4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c198834a4b47ea5a607dea3adae7823",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1705339f160bb1c11f1",
+    "url": "/static/css/main.8e49c9d7.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1705339f160bb1c11f1",
+    "url": "/static/js/main.53b7548e.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
